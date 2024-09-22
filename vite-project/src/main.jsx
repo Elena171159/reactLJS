4cfda604edc95d7de7ecb30f6e82942b885d1909
Elena Diachkova <zabava1711@bk.ru>
2024-09-22 14:59:51 +0300

    s

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -4,8 +4,12 @@ import { restaurants } from './mock'
 
 createRoot(document.getElementById('root')).render(
 <div>{restaurants.map((el) => {
-  return (<div><h1>{el.name}</h1><h3>Menu</h3><ul>{el.menu.map((item) => {
+  return (<div><h1>{el.name}</h1><h3>Menu</h3>
+  <ul>{el.menu.map((item) => {
     return (<div><li>{ item.name }</li><p>price: { item.price }</p><p>ingredients: {item.ingredients.join(' ')}</p></div>)
-  })}</ul><h3>Reviews</h3><ul>{el.reviews.map((review) => <li><p>{review.text}</p><p>stars: {review.rating}</p></li>)}</ul></div>)
+  })}</ul>
+  <h3>Reviews</h3>
+  <ul>{el.reviews.map((review) => <li><p>{review.text}</p><p>stars: {review.rating}</p></li>)}</ul>
+  </div>)
 })}</div>
 );
